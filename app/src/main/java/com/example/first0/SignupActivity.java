130ce404727c92ad3726fa806ec8af1683799e1d
JordanC <jordanchimborazo10@gmail.com>
2023-01-17 06:28:27 +0100

    configuracion de SignupActivity y xml

---
+++ app/src/main/java/com/example/first0/SignupActivity.java
@@ -0,0 +1,14 @@
+package com.example.first0;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SignupActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup);
+    }
+}
