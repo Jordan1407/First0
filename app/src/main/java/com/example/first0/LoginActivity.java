c67b2b2885f867d5fec357fdd04d4b91abb9b7d0
JordanC <jordanchimborazo10@gmail.com>
2023-01-17 04:53:18 +0100

    configuracion de LoginActivity

--- app/src/main/java/com/example/first0/LoginActivity.java
+++ app/src/main/java/com/example/first0/LoginActivity.java
@@ -2,7 +2,14 @@ package com.example.first0;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -10,5 +17,27 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+
+        /*cargar imagen con Glide (logo)
+        ImageView fondo = findViewById(R.id.logo);
+        Glide.with(this)
+                .load(R.drawable.error)
+                .transition(DrawableTransitionOptions.withCrossFade(100))
+                .centerCrop()
+                .placeholder(new ColorDrawable(this.getResources().getColor(R.color.color1)))
+                .circleCrop()
+                .into(fondo);*/
+
+        //animacion en logo
+        ImageView logo = findViewById(R.id.logo);
+        Animation anim = AnimationUtils.loadAnimation(this, R.anim.fadein);
+        logo.startAnimation(anim);
+
+        //open SignUp
+        Intent intent = new Intent(LoginActivity.this, SignupActivity.class);
+        startActivity(intent);
+
+
     }
 }
